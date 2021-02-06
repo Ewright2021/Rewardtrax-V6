a763398457cb937789343553d113a60c668e783d
Erika Wright <hey@ewright.me>
2021-02-06 15:02:45 -0500

    updated all link structures

--- src/js/shopping.js
+++ src/js/shopping.js
@@ -15,20 +15,58 @@ $.getJSON(
 
 // Populate Search Results Page
 $.getJSON(
-  "https://api.bestbuy.com/v1/products((categoryPath.id=pcmcat232900050000))?apiKey=wV0fRPELYjjeNtQRT7LmdGE4&sort=color.asc&show=categoryPath.id,categoryPath.name,color,customerReviewAverage,description,details.name,dollarSavings,features.feature,image,inStoreAvailability,longDescription,mobileUrl,modelNumber,name,onlineAvailability,onSale,percentSavings,regularPrice,salePrice,shipping,shippingCost,shortDescription,sku,thumbnailImage,url&pageSize=30&format=json",
+  "https://api.bestbuy.com/v1/products((categoryPath.id=pcmcat209000050006))?apiKey=wV0fRPELYjjeNtQRT7LmdGE4&sort=color.asc&show=categoryPath.id,categoryPath.name,color,customerReviewAverage,description,details.name,dollarSavings,features.feature,image,inStoreAvailability,longDescription,mobileUrl,modelNumber,name,onlineAvailability,onSale,percentSavings,regularPrice,salePrice,shipping,shippingCost,shortDescription,sku,thumbnailImage,url&pageSize=30&format=json",
   function (result) {
     let products = result.products;
     // console.log(products);
+    // Set Pagination
 
-    $(".search-results-item-title").each(function (i, featuredCategoryTitle) {
+    $(".search-results-item-title").each(function (i, title) {
       $(this).text(products[i].name);
-      // console.log(products[i].image);
     });
 
-    $(".search-item-thumbnail-img").each(function (i, prod) {
+    $(".search-item-thumbnail-img").each(function (i, image) {
       $(this).attr("src", products[i].image);
     });
 
-    $();
+    $(".search-results-item-price").each(function (i, price) {
+      let points = products[i].regularPrice;
+      $(this).prepend(points);
+    });
+    $(".in-store-pickup").each(function (i, pickup) {
+      let pick_up = products[i].inStoreAvailability;
+      if (!pick_up) {
+        $(this).addClass("d-none");
+      }
+    });
+    $(".on-sale").each(function (i, pickup) {
+      let onsale = products[i].onSale;
+      if (!onsale) {
+        $(this).addClass("d-none");
+      }
+    });
+
+    // Click to Item Details
+    $(".search-results-item-title").each(function (i, pickup) {
+      // let onsale = products[i].onSale;
+      $(this).click(function () {
+        alert.log("clicked");
+
+        // $(this).attr("src", products[i].image);
+
+        // window.location.href = "item-details.html";
+
+        // $(".img-main").attr("src", products[i].image);
+      });
+    });
+
+    // Click Search-Results-Item to Favorite
+    $(".bi-heart").each(function (i, pickup) {
+      // let onsale = products[i].onSale;
+      $(this).click(function () {
+        $(this).toggleClass("bi-heart");
+        $(this).toggleClass("bi-heart-fill text-danger");
+      });
+    });
   }
 );
